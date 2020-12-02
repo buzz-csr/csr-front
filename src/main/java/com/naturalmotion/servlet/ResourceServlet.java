78d0de433874bc62673fe8842328502a29d43246
Jerome <jerome.desachy@gmail.com>
2020-12-02 21:45:18 +0100

    Ajout encodage pour la réponse

--- src/main/java/com/naturalmotion/servlet/ResourceServlet.java
+++ src/main/java/com/naturalmotion/servlet/ResourceServlet.java
@@ -18,71 +18,67 @@ import com.naturalmotion.csr_api.service.updater.UpdaterException;
 
 public class ResourceServlet extends HttpServlet {
 
-    private static final long serialVersionUID = -2698675847367966977L;
+	private static final long serialVersionUID = -2698675847367966977L;
 
-    private final Logger log = LoggerFactory.getLogger(ResourceServlet.class);
+	private final Logger log = LoggerFactory.getLogger(ResourceServlet.class);
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+		resp.setContentType("application/json");
+		resp.setCharacterEncoding("UTF-8");
 
-        String type = req.getParameter("type");
-        if ("cash".equals(type)) {
-            addCashOr(req, resp);
-        } else if ("elite".equals(type)) {
-            addEliteTokens(req, resp);
-        } else {
-            addKeys(req, resp);
-        }
-    }
+		String type = req.getParameter("type");
+		if ("cash".equals(type)) {
+			addCashOr(req, resp);
+		} else if ("elite".equals(type)) {
+			addEliteTokens(req, resp);
+		} else {
+			addKeys(req, resp);
+		}
+	}
 
-    private void addEliteTokens(HttpServletRequest req, HttpServletResponse resp) {
-        try {
-            String path = new PathBuilder().build(req);
-            new ProfileUpdaterFileImpl(path).
-                    updateEliteTokens(new EliteTokenParamFactory().build(req));
+	private void addEliteTokens(HttpServletRequest req, HttpServletResponse resp) {
+		try {
+			String path = new PathBuilder().build(req);
+			new ProfileUpdaterFileImpl(path).updateEliteTokens(new EliteTokenParamFactory().build(req));
 
-            resp.getWriter().write(new NsbFormatter().getFileContent(path));
-        } catch (IOException | NsbException e) {
-            log.error("Error updating elite tokens", e);
-        }
-    }
+			resp.getWriter().write(new NsbFormatter().getFileContent(path));
+		} catch (IOException | NsbException e) {
+			log.error("Error updating elite tokens", e);
+		}
+	}
 
-    public void addKeys(HttpServletRequest req, HttpServletResponse resp) {
-        try {
-            String bronze = req.getParameter("bronze");
-            String gold = req.getParameter("gold");
-            String silver = req.getParameter("silver");
-            String path = new PathBuilder().build(req);
+	public void addKeys(HttpServletRequest req, HttpServletResponse resp) {
+		try {
+			String bronze = req.getParameter("bronze");
+			String gold = req.getParameter("gold");
+			String silver = req.getParameter("silver");
+			String path = new PathBuilder().build(req);
 
-            ProfileUpdater profileUpdater = new ProfileUpdaterFileImpl(path);
-            profileUpdater.updateResource(ResourceType.BRONZE_KEY, new BigDecimal(bronze));
-            profileUpdater.updateResource(ResourceType.GOLD_KEY, new BigDecimal(gold));
-            profileUpdater.updateResource(ResourceType.SILVER_KEY, new BigDecimal(silver));
+			ProfileUpdater profileUpdater = new ProfileUpdaterFileImpl(path);
+			profileUpdater.updateResource(ResourceType.BRONZE_KEY, new BigDecimal(bronze));
+			profileUpdater.updateResource(ResourceType.GOLD_KEY, new BigDecimal(gold));
+			profileUpdater.updateResource(ResourceType.SILVER_KEY, new BigDecimal(silver));
 
-            resp.getWriter().write(new NsbFormatter().getFileContent(path));
-        } catch (IOException
-                | UpdaterException
-                | NsbException e) {
-            log.error("Error adding keys", e);
-        }
-    }
+			resp.getWriter().write(new NsbFormatter().getFileContent(path));
+		} catch (IOException | UpdaterException | NsbException e) {
+			log.error("Error adding keys", e);
+		}
+	}
 
-    public void addCashOr(HttpServletRequest req, HttpServletResponse resp) {
-        try {
-            String cash = req.getParameter("cash");
-            String gold = req.getParameter("gold");
-            String path = new PathBuilder().build(req);
+	public void addCashOr(HttpServletRequest req, HttpServletResponse resp) {
+		try {
+			String cash = req.getParameter("cash");
+			String gold = req.getParameter("gold");
+			String path = new PathBuilder().build(req);
 
-            ProfileUpdater profileUpdater = new ProfileUpdaterFileImpl(path);
-            profileUpdater.updateResource(ResourceType.CASH, new BigDecimal(cash));
-            profileUpdater.updateResource(ResourceType.GOLD, new BigDecimal(gold));
-
-            resp.getWriter().write(new NsbFormatter().getFileContent(path));
-        } catch (IOException
-                | UpdaterException
-                | NsbException e) {
-            log.error("Error adding cash/or", e);
-        }
-    }
+			ProfileUpdater profileUpdater = new ProfileUpdaterFileImpl(path);
+			profileUpdater.updateResource(ResourceType.CASH, new BigDecimal(cash));
+			profileUpdater.updateResource(ResourceType.GOLD, new BigDecimal(gold));
+			resp.getWriter().write(new NsbFormatter().getFileContent(path));
+		} catch (IOException | UpdaterException | NsbException e) {
+			log.error("Error adding cash/or", e);
+		}
+	}
 
 }
